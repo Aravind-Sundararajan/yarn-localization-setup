c6b32c83869036dca99fd830fc26cf4057af556b
Aravind-Sundararajan <aravind.sundararajan@protonmail.com>
2021-07-17 16:36:36 -0400

    output looks ok

--- setup_locales.cpp
+++ setup_locales.cpp
@@ -1,40 +1,74 @@
 #include "setup_locales.h"
 
-//uncomment to output to file
+//comment to output to cout
 #define USE_OUTPUT_FILE 
 
 #ifdef USE_OUTPUT_FILE
-#define OSTRM outfile
+#define OSTRM out_yarn
+#define CSVSTRM out_csv
 #else
 #define OSTRM cout
+#define CSVSTRM cout
 #endif
 
 using namespace std;
 
 int main(int argc, char **argv){
 
-    istringstream issline, iss;
-    string line, str;
+    string line;
+    std::stringstream buf;
+    int c;
+    bool in_body;
+    char delim = ',';
+    string body_start = "---";
+    string body_end   = "===";
 
-    if (argc != 3){
-        std::cout << "usage: ./setuplocales input-file output-file" << std::endl;
+    if (argc != 4){
+        std::cout << "usage: ./setuplocales input-file metatagged-file csv-file" << std::endl;
     }
-    ifstream file(argv[1]);
+    string fpath = string(argv[1]);
+    ifstream file(fpath);
     
     #ifdef USE_OUTPUT_FILE
-    ofstream outfile(argv[2]);
-    if (!outfile.is_open()) {
+    ofstream out_yarn(argv[2]);
+    if (!out_yarn.is_open()) {
         cerr << "Output file isn't open" << endl;
         exit(1);
     }
+
+    ofstream out_csv(argv[3]);
+    if (!out_csv.is_open()) {
+        cerr << "Output csv isn't open" << endl;
+        exit(1);
+    }
     #endif
-    
-    while (getline(file, line) && !line.empty()) {
+
+    in_body = false;
+    c = 0;
+    while (getline(file, line)) {
         
+        if (line == body_end){
+            in_body = false;
+        }
+
+        if (in_body){
+            c++;
+            buf << "#line:" << std::hex << std::hash<std::string>{}(line + fpath + std::to_string(c));
+            OSTRM << line << buf.str() << endl;
+            CSVSTRM << buf.str() << delim << line << endl;
+            buf.str("");
+        }else{
+            OSTRM << line << endl;
+        }
+
+        if (line == body_start){
+            in_body = true;
+        }
     }
 
     #ifdef USE_OUTPUT_FILE
-    outfile.close();
+    out_yarn.close();
+    out_csv.close();
     #endif
 
     return 0;
--- setup_locales.h
+++ setup_locales.h
@@ -4,9 +4,7 @@
 #include <iostream>
 #include <iomanip>
 #include <sstream>
-#include <cstdint>
 #include <string>
 #include <stdio.h>
 #include <stdlib.h>
-#include <chrono>
 #endif
